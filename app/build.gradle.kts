5be902d16b5b137ba6e17f91e9c223dd497c4e54
Frank Massi <frankmassi@gmail.com>
2019-09-20 13:40:37 -0500

    Convert build scripts to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.frankmassi.posturereminder"
+        minSdkVersion(28)
+        targetSdkVersion(28)
+        versionCode = 5
+        versionName ="0.0.1"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled=false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildToolsVersion = "28.0.3"
+
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(kotlin("stdlib-jdk7", project.rootProject.extra["kotlinVersion"].toString()))
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.core:core-ktx:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.work:work-runtime-ktx:${project.rootProject.extra["workVersion"]}")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    androidTestImplementation("androidx.work:work-testing:${project.rootProject.extra["workVersion"]}")
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.register("tagGithub", Exec::class) {
+    workingDir( "../bin")
+    executable("python3")
+    args ("tag_release.py", android.defaultConfig.versionName + "-" + android.defaultConfig.versionCode)
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.50'
     repositories {
         google()
         jcenter()
-        
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.5.0")
+        classpath(kotlin("gradle-plugin", version = "1.3.50"))
+
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
 
@@ -19,11 +18,14 @@ allprojects {
     repositories {
         google()
         jcenter()
-        
+    }
+
+    extra.apply {
+        set("workVersion", "2.2.0")
+        set("kotlinVersion", "1.3.50")
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
-
