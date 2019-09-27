4e6f35290f55b6140b427f762c09e1b22abc6017
Frank Massi <frankmassi@gmail.com>
2019-09-27 09:58:38 -0500

    Gradle updates - version and dependencyUpdate task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("kotlin-android-extensions")
+    id("com.github.ben-manes.versions")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
@@ -8,6 +10,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.0")
         classpath(kotlin("gradle-plugin", version = "1.3.50"))
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.25.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
@@ -28,4 +31,14 @@ allprojects {
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+
+tasks.withType<DependencyUpdatesTask> {
+    // optional parameters
+    checkForGradleUpdate = true
+    revision = "release"
+    outputFormatter = "json"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "report"
 }
